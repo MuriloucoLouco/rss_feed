459900b2383078f46652c730518e406a598b8568
MuriloucoLouco <murilouko.louco@gmail.com>
2020-08-18 14:33:56 -0400

    Tentando consertar erro no heroku

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+app.set('port', (process.env.PORT || 3000));
 
 app.use(routes);
 
-app.listen(3000);
+app.listen(app.get('port'), () => {
+    console.log('Servidor rodando na porta ' + app.get('port'));
+});

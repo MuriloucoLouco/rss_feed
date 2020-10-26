115984b5d5b713d551a4ccdfff6b8c5557355c1c
muriloucolouco <murilouko.louco@gmail.com>
2020-10-26 16:32:18 -0400

    Melhorando estutura e UI

--- fetch_all.js
+++ fetch_all.js
@@ -9,21 +9,32 @@ const internetisbeatiful = 'https://www.reddit.com/r/InternetIsBeautiful/.rss';
 
 async function fetch_all() {
     console.log('Carregando novos links...');
-
-    try {
-        // cosenza_list = await fetch_animelist(cosenza);
-        luke_smith_videos = await fetch_invidious(luke_smith);
-        mental_outlaw_videos = await fetch_invidious(mental_outlaw);
-        devto_posts = await fetch_devto(devto);
-        freecodecamp_posts = await fetch_freecodecamp(freecodecamp);
-        internetisbeatiful_posts = await fetch_reddit(internetisbeatiful);
-    } catch (error) {
-        console.log(error);
-    }
-
-    console.log('Terminado de carregar.');
-
-    return {luke_smith_videos, mental_outlaw_videos, devto_posts, freecodecamp_posts, internetisbeatiful_posts};
+	
+	rss_json = new Object();
+	// cosenza_list = await fetch_animelist(cosenza);
+	try {
+		luke_smith_videos = await fetch_invidious(luke_smith);
+		rss_json = {...rss_json, 'Luke Smith videos': luke_smith_videos};
+	} catch (error) { console.log(error) };
+	try {
+		mental_outlaw_videos = await fetch_invidious(mental_outlaw);
+		rss_json = {...rss_json, 'Mental Outlaw videos': mental_outlaw_videos};
+	} catch (error) { console.log(error) };
+	try {
+		devto_posts = await fetch_devto(devto);
+		rss_json = {...rss_json, 'Devto posts': devto_posts};
+	} catch (error) { console.log(error) };
+	try {
+		freecodecamp_posts = await fetch_freecodecamp(freecodecamp);
+		rss_json = {...rss_json, 'Freecodecamp posts': freecodecamp_posts};
+	} catch (error) { console.log(error) };
+	try {
+		internetisbeatiful_posts = await fetch_reddit(internetisbeatiful);
+		rss_json = {...rss_json, 'InternetIsBeautiful posts': internetisbeatiful_posts};
+	} catch (error) { console.log(error) };
+	
+	console.log('Terminado de carregar.')
+	return rss_json;
 }
 
 module.exports = fetch_all;
